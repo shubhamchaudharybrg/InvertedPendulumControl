ec629eb77a4213877c87cd856c748e3391840a07
Shubham Chaudhary <shubhamch@iiitd.ac.in>
2021-05-01 10:50:46 +0530

    Fix pend intial position and control loss not working bug

--- controller.py
+++ controller.py
@@ -9,11 +9,11 @@ from pyconsys.PIDControl import PIDControl
 IP_ADDRESS = "127.0.0.1"
 PORT = 5432
 DISCONNECT_MESSAGE = "DISCONNECT"
-ACKNOWELEDGE_MESSAGE = "ACK"
-RTS = "RTS"
-CTS = "CTS"
+# ACKNOWELEDGE_MESSAGE = "ACK"
+# RTS = "RTS"
+# CTS = "CTS"
 MESSAGE_LENGTH = 128
-PID_CONTROL = PIDControl(105, 83, 28)  # Kp, Ki, Kd
+PID_CONTROL = PIDControl(105, 100, 60)  # Kp, Ki, Kd 105, 83, 28
 
 pendulumAngle = 0 # Receives from simulated pendulum
 
@@ -32,13 +32,26 @@ def getControl(angle):
     # print(f"control calculated : {control}") 
     return control
 
+# count = 0
+# data = 0
+
 while True:
 # for ii in range(10):
-    if random.randint(1, 1000) == 8:
-        time.sleep(20)
-
-    _angle = float(connection.recv(MESSAGE_LENGTH).decode())
+    # print(f"Data Before : {data}")
+    data = connection.recv(MESSAGE_LENGTH).decode()
+    # print(f"Data After : {data}")
     
-    ctrl = getControl(_angle)
-    print(f"angle : {_angle} , control : {ctrl}")
-    connection.send(bytes(str(round(ctrl, 10)), "utf-8"))
+    if data == DISCONNECT_MESSAGE:
+        break
+    else:
+        # if count <= 300: 
+
+        controlData = getControl(float(data))
+        print(f"angle : {data} , control : {controlData}")
+        connection.send(bytes(str(round(controlData, 10)), "utf-8"))
+        
+        # elif count > 450:
+            # count = 0
+    # print(count)
+    # count += 1
+connection.close()
