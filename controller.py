891b004b68d3d11b504f72a9bfc61935857b02ca
Shubham Chaudhary <shubhamch@iiitd.ac.in>
2021-05-07 14:44:18 +0530

    Implement latency case

--- controller.py
+++ controller.py
@@ -8,15 +8,14 @@ from pyconsys.PIDControl import PIDControl
 import simulatedScenarios as ss
 import numpy as np
 
-IP_ADDRESS = "127.0.0.1"
-PORT = 1234
+IP_ADDRESS = "192.168.43.98" #"192.168.137.1"
+PORT = 12345
 DISCONNECT_MESSAGE = "DISCONNECT"
 MESSAGE_LENGTH = 130 #13
-PID_CONTROL = PIDControl(105, 83, 28)  # Kp, Ki, Kd 105, 83, 28
-SIMULATION_TIME = 25 # in seconds
+PID_CONTROL = PIDControl(105, 83, 28)  # Kp, Ki, Kd
 angList = []
-max = 0
-introducedLatency = 0.0162
+_max = 0
+introducedLatency = 0.014
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP_ADDRESS,PORT))
@@ -35,35 +34,28 @@ print("Connected")
 
 ts = time.time(); te = time.time()
 while True:
-    print(f"te-ts : {te-ts}")
+    # print(f"te-ts : {te-ts}")
     if te-ts > SIMULATION_TIME:
         break
-# for ii in range(10):
-    # if random.randint(1, 10) == 8:
-        # time.sleep(.2)
 
-    time.sleep(introducedLatency)    # 0.016 gives jittery behaviour
+    time.sleep(introducedLatency)  # Introduces latency 
 
     ang = connection.recv(MESSAGE_LENGTH).decode()
     if ang == DISCONNECT_MESSAGE:
         break
    
     ang = ang.split('/n')[:-1:1]
-    if len(angList) < 50:
-        angList.extend(ang)
-
-    # print(f"ang : {ang}")
-    # print(f"angList : {angList}")
-    print(f"Length of list : {len(angList)}")
+    
+    angList.extend(ang)
 
     _angle = float(angList.pop(0))
-    if abs(_angle) > max:
-        max = abs(_angle)
+    if abs(_angle) > _max:
+        _max = abs(_angle)
     
     ctrl = getControl(_angle)
     print(f"angle : {_angle} , control : {ctrl}") # Angle in Radian
     connection.send(bytes(str(round(ctrl, 10)), "utf-8"))
     te = time.time()
     
-print(f"Max Angle : {max*180/np.pi}; Latency : {introducedLatency}")
+print(f"Max Angle : {_max*180/np.pi}; Latency : {introducedLatency}")
 connection.close()
